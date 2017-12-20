8cfe7ce761e4c64252d14fa7359cf751c3fb1bc2
hoanganh <hoanganhA91013@gmail.com>
2017-12-20 22:47:02 +0700

    Modify authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
   get 'home/index'
-  root 'home#index'
+  devise_scope :user do
+    authenticated :user do
+      root 'home#index', as: :authenticated_root
+    end
+  
+    unauthenticated do
+      root 'devise/sessions#new', as: :unauthenticated_root
+    end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
